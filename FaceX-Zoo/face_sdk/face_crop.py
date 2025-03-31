9fded09bf99ce94eb81452681dd4c15bd1f3bc44
Kitkacy <157039998+Kitkacy@users.noreply.github.com>
2025-03-31 21:13:00 +0200

    sad emoji

--- FaceX-Zoo/face_sdk/face_crop.py
+++ FaceX-Zoo/face_sdk/face_crop.py
@@ -10,7 +10,7 @@ import cv2
 from core.image_cropper.arcface_cropper.FaceRecImageCropper import FaceRecImageCropper
 
 if __name__ == '__main__':
-    image_path = 'api_usage/test_images/test1.jpg'
+    image_path = './Untitled.jpeg'
     image_info_file = 'api_usage/test_images/test1_landmark_res0.txt'
     line = open(image_info_file).readline().strip()
     landmarks_str = line.split(' ')
@@ -20,3 +20,4 @@ if __name__ == '__main__':
     image = cv2.imread(image_path)
     cropped_image = face_cropper.crop_image_by_mat(image, landmarks)
     cv2.imwrite('api_usage/temp/test1_cropped.jpg', cropped_image)
+
